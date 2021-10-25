2105088fbce997e00512720dded4181214a611e2
SteveOntheMoon <73540419+SteveontheMoon@users.noreply.github.com>
2021-10-25 11:12:45 +0200

    to compile use -lpthread

--- L3/threadtest.c
+++ L3/threadtest.c
@@ -4,7 +4,9 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
-die(char * s, int e){
+/* compilazione: gcc -lpthread -o <output> threadtest.c */
+
+int die(char * s, int e){
     /* stampa gli errori ed esce (non si puo' usare perror) */
     printf("%s [%i]\n",s,e);
     exit(1);
@@ -23,19 +25,20 @@ void * codice_thread(void * a){
     pthread_exit(NULL);
 }
 
-main(){
+int main(){
     pthread_t tid[2];
     int i,err;
 
     /* crea i thread */
     for (i=0;i<2;i++)
-      if (err=pthread_create(&tid[i],NULL,codice_thread,NULL))
-         die("errore create",err);
+      if (err==pthread_create(&tid[i],NULL,codice_thread,NULL))
+         die("Errore create",err);
                            
          /* attende i thread. Non si legge il valore di ritorno (secondo parametro e' NULL) */
          for (i=0;i<2;i++)
-           if (err=pthread_join(tid[i],NULL))
+           if (err==pthread_join(tid[i],NULL))
               die("Errore join",err);
                                                      
            printf("I thread hanno terminato l'esecuzione correttamente\n");
+           return 0;
 }
