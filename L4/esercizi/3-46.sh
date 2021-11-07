00c8fb615c13ad83b2ec14eefdf6ff1a6ff9f564
SteveOntheMoon <73540419+SteveontheMoon@users.noreply.github.com>
2021-11-07 15:36:29 +0100

    Sistemato formattazione esercizio 3-46

--- L4/esercizi/3-46.sh
+++ L4/esercizi/3-46.sh
@@ -4,6 +4,6 @@
 ps -eo uname,pid,ppid,args | sort -nk3 | tail -n1
 #   ^ list all process
 #	 ^ format output
-#								   ^ numerical sort
-#									^ increasing numerical sorting based on content of column 3
-#											   ^ take last line from file tail 
+#				   ^ numerical sort
+#				    ^ increasing numerical sorting based on content of column 3
+#					       ^ take last line from file tail 
