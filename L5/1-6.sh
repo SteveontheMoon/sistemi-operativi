0450b75122941d9c56024824ca58b707a38ea018
SteveOntheMoon <73540419+SteveontheMoon@users.noreply.github.com>
2021-11-09 00:18:40 +0100

    come recupero delle cartelle cancellate con il comando rm?

---
+++ L5/1-6.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Trova file piu' grande (in byte) in una data cartella
+find ~/Documents/ -type f -print0 | xargs -0 du -sbh | sort -rh | head -1
+#find -type f : prendi solamente file, 'h' mostra risultato senza spazi
+#args -0 : passiamo agomenti a du
+#du -sbh : 's' per 'summarize', 'b' [..], 'h' per 'human readable'.
+#sort -rh : 'rh' per riordinare in reverso numeri 'umani' passati in input
+#head -1 : mostra una sola riga.
+
+# Ma che sbatti mi sono fatto?
